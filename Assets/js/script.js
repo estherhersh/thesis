838638da1be896dda7a1fd617a33e51c009ce1df
estherhersh <estherhersh@gmail.com>
2017-03-28 15:27:51 -0400

    update UI

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -34,7 +34,6 @@ function submit_code()
 
 }
 
-
  codeeditor.on('change', function(e) {
     setTimeout(submit_code, 5000);
 });
@@ -47,30 +46,62 @@ $(document).ready(function(){
     $("#codeeditor").draggable();
 
 
-
-
-  //    var sidebarWidth = $('.sidebar').width();//get width automaticly
-  // $('.sidebar').hover(function() {
-  //   if($(this).css("margin-right") == -200+"px" && !$(this).is(':animated'))
-  //   {
-  //       $('.sidebar').animate({"margin-right": '-='+100});
-  //   }
-  //   else
-  //   {
-  //       if(!$(this).is(':animated'))//perevent double click to double margin
-  //       {
-  //           $('.sidebar').animate({"margin-right": '+='+100});
-  //       }
-  //   }
-
-
-  // });
-
 });
 
-// $( document ).click(function() {
-//   $( "#versions" ).animate({"margin-right": '-=200'});
-// });
+// Code to add textboxes
+// ---------------------------------------------
+
+function addText() {
+  // Create a new div 
+  var maincanvas= document.getElementById("maincanvas")
+  var textbox = document.createElement('div')
+  textbox.className = 'textbox';
+  textbox.innerHTML = '<textarea id=text rows="1" cols="50" placeholder="Type Here..."></textarea>'
+  // Add it to the main canvas
+  maincanvas.appendChild(textbox);
+  $(textbox).draggable({cursor: "crosshair"})
+
+}
+
+var textButton = document.getElementById('textbutton');
+textButton.addEventListener('click', addText);
+
+// Code to add questions
+// ---------------------------------------------
+
+function addQuestion() {
+  // Create a new div 
+  var maincanvas= document.getElementById("maincanvas")
+  var questbox = document.createElement('div')
+  questbox.className = 'questbox';
+  questbox.innerHTML = '<textarea id=question rows="1" cols="50" placeholder="QUESTION"></textarea><br><textarea id=answer rows="6" cols="50" placeholder="Type Here..."></textarea>'
+  // Add it to the main canvas
+  maincanvas.appendChild(questbox);
+  $(questbox).draggable({cursor: "crosshair"})
+
+}
+
+var questButton = document.getElementById('quest');
+questButton.addEventListener('click', addQuestion);
+
+// Code to add Arduino
+// ---------------------------------------------
+
+function addArduino() {
+  // Create a new div 
+  var maincanvas= document.getElementById("maincanvas")
+  var arduino = document.createElement('div')
+  arduino.className = 'arduinos';
+  arduino.innerHTML = '<img src="assets/images/arduino.png" width=400px/>'
+  // Add it to the main canvas
+  maincanvas.appendChild(arduino);
+  $(arduino).draggable({cursor: "crosshair"})
+
+}
+
+var arduinobutton = document.getElementById('arduino');
+arduinobutton.addEventListener('click', addArduino);
+
 
 
 // Code to handle versions
