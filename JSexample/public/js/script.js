848d8081dfbfcb93461b66b86ca900d945315fd5
estherhersh <estherhersh@gmail.com>
2017-04-26 09:31:32 -0400

    add JS

---
+++ JSexample/public/js/script.js
@@ -0,0 +1,298 @@
+
+
+
+//Create text editor
+// var basicElements=[];
+var code
+var data_url
+var textarea = document.getElementById("codeeditor")
+var codeeditor = CodeMirror.fromTextArea(textarea, {
+  mode:  "javascript",
+  styleActiveLine: true,
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseTags: true,
+  autoCloseBrackets: true,
+  theme : 'solarized',
+  extraKeys: {
+    "Tab": "indentMore"
+  }
+});
+
+var currentVersion = '';
+var currentLevel = -1;
+
+//update code on change (only updates first time)
+$( "#codeeditor").change(submit_code());
+
+
+function submit_code()
+{
+   var code = `
+
+           ${codeeditor.getValue()} 
+
+
+    `
+
+    var previewFrame = document.getElementById('result');
+    var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
+    preview.open();
+    preview.write(code);
+    preview.close();
+}
+
+// setInterval(submit_code, 10000)
+
+
+//  CRASHES  WITH FOR LOOPS
+//  codeeditor.on('change', function(e) {
+//     setTimeout(submit_code, 5000);
+// });
+
+
+// Code to handle versions
+// ---------------------------------------------
+
+var counter = 1;
+
+
+function createVersion(id,level,parentId) {
+  row = $('#sidebar-row-'+level)
+  if(!row.length){
+    row = $(`<div id="sidebar-row-${level}" class="sidebar-level"></div>`)
+    $('.sidebar').append(row)
+    // versionsCol.appendChild(row)
+  }
+
+
+  versionElement= $(`<input id="${id}" class="sidebar-version" data-level="${level}" placeholder= Version${counter}></input>`).click(function(e){
+    // console.log("clicked")
+    selectVersion($(this).attr('id'), $(this).data('level'))
+  })
+  $(row).append(versionElement)
+
+
+  parentElement = $(`#${parentId}`)
+  // console.log(parentElement)
+  // console.log(currentDate)
+
+//add connections 
+      $(`#${parentId}`).connections({
+    to: `#${id}`,
+    'class': 'related'
+  });
+  // console.log('connect')
+  $(`#${parentId}`).connections('update');
+
+  var c = $('connection');
+  setInterval(function() {
+    c.connections('update');
+  }, 10);
+  // Add it to the #versions column 
+  // versionsCol.appendChild(version);
+
+
+
+
+  counter++;
+}
+
+  function selectVersion(id, level){
+  // make a get request to /api/versions/{id}
+  $('.sidebar-version').removeClass('selected')
+  $(`#${id}`).addClass('selected')
+
+  $.ajax({
+    type: "GET",
+    url: 'http://localhost:8080/api/versions/'+id,
+    contentType : 'application/json',
+    success: function(data) {
+      currentVersion = id;
+      currentLevel = level;
+
+      var code = `
+         
+             ${data.editorValue} 
+      
+   `
+        
+        // take the result data and put data.editorValue inside the editor
+      codeeditor.setValue(data.editorValue);  
+      var previewFrame = document.getElementById('result');
+      var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
+      preview.open()
+      preview.write(code)
+      preview.close()
+
+      $(".questbox").empty();
+      //run through the array of basic elements and draw them to the canvas
+      for(i=0;i<2;i++){
+        addQuestion(data.basicElements[i].question,data.basicElements[i].answer)
+        console.log(data.basicElements[i].question,data.basicElements[i].answer)
+        // addQuestion(656,435)
+      }
+    }
+  });
+}
+
+
+function loadChildren(data, id, level){
+  let childrenArray = data.filter(function(d){return d.parentId == id })
+  $.each(childrenArray, function(i, d){
+    createVersion(d._id, level,id)
+    loadChildren(data, d._id, level+1)
+  })
+}
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+// Load existing versions
+// ---------------------------
+
+if(getUsernameFromCookie(document.cookie) != ""){
+  username= getUsernameFromCookie(document.cookie)
+}else{
+  var username= makeid();
+}
+console.log(username)
+
+
+var exp = new Date();     //set new date object  
+exp.setTime(exp.getTime() + (1000 * 60 * 60 * 24 * 30));     //set it 30 days ahead 
+// var username= 'user1'
+// cookieuser= getCookie('cookie')
+// cookieuser= username
+
+
+
+function setCookie(name, value, expires) {  
+
+  document.cookie = escape(name) + "=" + escape(value) + "; expires=" + expires.toGMTString(); 
+  // alert(getUsernameFromCookie(document.cookie))
+} 
+
+function getUsernameFromCookie(cookie_){
+  
+  var pos = cookie_.indexOf('username')
+  cookie_=cookie_.substring(pos);
+
+  var pos = cookie_.indexOf('=')
+  cookie_=cookie_.substring(pos+1);
+
+  var pos = cookie_.indexOf(';')
+  if(pos>0){
+    cookie_=cookie_.substring(0, pos);
+  }
+  return cookie_.substring(pos);
+}
+
+
+
+setCookie('username', username, exp);
+// console.log(typeof(document.cookie))
+console.log(document.cookie)
+
+
+
+
+// var data = {username: username};
+
+// TODO: Add username to data
+ $.ajax({
+    type: "GET",
+    url: 'http://localhost:8080/api/versions',
+    // data:{ username : 'username2'},
+    success: function(data) {
+
+      // data.find({username:username})
+      var user = data.filter(function(d){return d.username == username });
+            console.log(user)
+
+    // var user=[];
+    //   for(i=0;i<data.length;i++){
+    //   user.push( data.filter(function(d){return d.username == "user1" }))
+    //   }
+      
+      let parentArray = user.filter(function(d){return d.parentId == "" })
+      $.each(parentArray, function(j, p){
+        createVersion(p._id, 0, "")
+        loadChildren(data, p._id, 1)
+      })
+
+
+      // id = latest timestamp in data array
+      // selectVersion(id)
+    }
+  
+  });
+
+// Create new versions
+// ---------------------------
+
+// Find the save button
+var saveButton = document.getElementById('singlebutton');
+
+// Find the column to put the versions in
+// var versionsCol = document.getElementById('versions');
+
+// When SAVE button is clicked
+saveButton.addEventListener('click', function(e) {
+  // Don't follow the link
+  e.preventDefault();
+
+  // Grab
+  var questions = document.querySelectorAll('.questbox');
+  var question = document.querySelectorAll('#question');
+  var answer = document.querySelectorAll('#answer');
+
+    var questionsData=[];
+
+  for(var i = 0; i < questions.length; i++) {
+        // var top= $(questions[i]).position().top 
+        // console.log(top)
+    questionsData.push({
+      // top: $(questions[i]).offset().top ,
+      // left: $(questions[i]).offset().left, 
+      answer: $(answer[i]).val(), 
+      question: $(question[i]).val() 
+    })
+  }
+
+
+  // save a version
+  $.ajax({
+    type: "POST",
+    url: 'http://localhost:8080/api/versions',
+    contentType : 'application/json',
+    data: JSON.stringify({
+      editorValue: codeeditor.getValue(),
+      basicElements: questionsData,
+      parentId: currentVersion,
+      username: username
+      // date: Date 
+
+          }),
+    success: function(data) {
+      // THE ID IS NOT THERE!
+      currentVersion = data._id;
+      currentLevel++;
+      createVersion(data._id, currentLevel, data.parentId);
+      // console.log(basicElements)
+    }
+
+
+  });
+    window.location.reload();
+
+
+})
+
---
+++ JSexample/server.js
@@ -0,0 +1,188 @@
+console.log('server is starting');
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+//sessions
+var session = require('express-session')
+var expressValidator= require('express-validator')
+
+
+
+// ///////////////////////////////////
+
+var server= app.listen(8080, listening);
+
+function listening(){
+    console.log("listening...");
+}
+
+//allows express to hold static files like html(scif)
+app.use(express.static('public'));
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//setup validator here after body parser
+app.use(expressValidator());
+app.use(session({cookie:{maxAge:6000},secret:'secret',saveUninitialized:true,resave:true}));
+
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// //middleware for sessions
+// app.use(express.cookieParser('S3CRE7'));
+// app.use(express.cookieSession());
+// app.use(app.router);
+
+// var port = process.env.PORT || 8080;        // set our port
+
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/versions'); // connect to our database
+
+var Version     = require('./app/models/version');
+
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    // console.log("0")
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    // if(!req.session.visitcount){
+    //     req.session.visitcount=1;
+    // }else{
+    //     req.session.visitcount++;
+    // }
+    // res.render('index',{title:'Express'})
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+// more routes for our API will happen here
+
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/versions')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        // console.log(req.body)
+        console.log("creating a version")
+        var version = new Version();      // create a new instance of the Bear model
+        version.editorValue = req.body.editorValue;  // set the bears name (comes from the request)
+        version.basicElements= req.body.basicElements;
+        version.parentId = req.body.parentId
+        version.id= req.body._id
+        version.username= req.body.username;
+        // version.bearType = req.body.bearType;  // set the bears type (comes from the request)
+
+        // save the bear and check for errors
+        version.save(function(err) {
+             version.id= req.body._id
+            console.log(version.id)
+            console.log("callback from save")
+            console.log(err);
+            if (err)
+                res.json({error:err});
+
+            res.json({ _id: version.id});
+        });
+        
+    });
+    //get all the bears 
+router.route('/versions').get(function(req,res){
+        // only find by username
+        Version.find(function(err,versions){
+            if (err)
+                res.send(err);
+
+            res.json(versions);
+        });
+    });
+
+
+///bears that end in ber_id
+router.route('/versions/:version_id')
+
+    .get(function(req, res) {
+        // console.log('get ID')
+        Version.findById(req.params.version_id, function(err, version) {
+            if (err)
+                res.send(err);
+            res.json(version);
+        });
+    })
+//update the bear with the ID that you just pulled
+    .put(function(req, res) {
+        // use our model to find the bear we want 
+        Version.findById(req.params.version_id, function(err, version) {
+            if (err)
+                res.send(err);
+        
+        version.editorValue = req.body.editorValue;  // update the bears info
+        version.basicElements= req.body.basicElements;
+        version.parentId = req.body.parentId;
+
+
+
+         // save the bear
+            version.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'version updated!' });
+
+            });
+
+        });
+
+    })
+
+// delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function(req, res) {
+        Version.remove({
+            _id: req.params.version_id
+        }, function(err, version) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+
+app.get('/', sendVersion);   
+
+
+function sendVersion(req, res) {
+    res.sendFile("public/index.html", { root: __dirname }); 
+}
+
+
+
+// START THE SERVER
+// =============================================================================
+// app.listen(port);
+// console.log('Magic happens on port ' + port);
+
+
